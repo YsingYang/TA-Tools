e92f33d6b057dd1a9f0ae2efbf961b86f8ca094f
YsingYang <ysingemail@gmail.com>
2017-10-18 19:59:36 +0800

    [修改] 程序结构

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ from unpack_Tools import *
 
 if __name__ == '__main__':
     tool = Tools()
-    #tool.init_student_list('/home/ysing/Documents/TA-MAD/名单/17.1手机平台（理论+实验）名单.xls')
-    #result = tool.get_homework_result(['/home/ysing/Documents/TA-MAD/Homework-zip/Lab-4-zip/**/*.zip', \
-    #                                   '/home/ysing/Documents/TA-MAD/Homework-zip/Lab-4-zip/**/*.rar'])
-    #for row in range(0, len(result), 3):
-    #    print(result[row:row+3])
+    tool.init_student_list('/home/ysing/下载/手机平台应用开发.xls', 1, 6)
+    result = tool.get_homework_result('/home/ysing/PycharmProjects/TA-tool/Lab_4')
+    for row in range(0, len(result), 3):
+        print(result[row:row+3])
 
     tool.create_dist_dir('/home/ysing/Documents/TA-MAD/Homework-zip/Lab-4-Homework')
-    tool.unpack_rar('./Lab_4/**/*.rar')
-    # tool.unpack_zip('/home/ysing/Documents/TA-MAD/Homework-zip/Lab-4-zip/**/*.zip') #搜索zip路径
-    # tool.copy_specific_type('/home/ysing/Documents/TA-MAD/Homework-zip/Lab-4-Homework', \
-    #                         remove=True, types = ['*.pdf', '*.java'], exclude=['ExampleUnitTest.java', 'ExampleInstrumentedTest.java'])
+    tool.unpack('/home/ysing/PycharmProjects/TA-tool/Lab_4')
+    tool.copy_specific_type('/home/ysing/Documents/TA-MAD/Homework-zip/Lab-4-Homework', \
+                             remove=True, types = ['*.pdf', '*.java'], exclude=['ExampleUnitTest.java', 'ExampleInstrumentedTest.java'])
--- unpack_Tools.py
+++ unpack_Tools.py
@@ -5,77 +5,39 @@ import rarfile
 import re
 import zipfile
 import xlrd
-'''
-def check_is_not_MainActivity(type, file_path):
-    if(type == types[1] and re.search('/app/src', file_path) == None):
-        return True #如果不存在app/src, 则输出文件返回True
-    if(re.search('ExampleUnitTest.java', file_path) != None or re.search('ExampleInstrumentedTest.java', file_path) != None):
-        return True #如果是test文件夹中的内容返回true
-    return False
-
-def delete_dir(path, sub_file_list):
-    for sub_file in sub_file_list: #注意列出来的只是一个文件名, 而不是一个目录
-        if os.path.isdir(path + '/' + sub_file): #如果是目录, 则删除,
-            shutil.rmtree(path + '/' + sub_file)
-
-zip_list = glob.glob('./**/*.zip', recursive=True)
-regex_pattern = re.compile(r'\d{8}')
-for zip_file in zip_list:
-    #首先创建相应学号的文件夹
-    sid = regex_pattern.findall(zip_file)
-    if(not len(sid) == 1): #如果获取到的sid不等于1
-        print('正则匹配出现异常, 该文件名为 {}'.format(zip_file))
-        continue
-
-    # 指定文件中创建目录
-    student_dir = '../Lab_4/' + sid[0]
-    try:
-        os.makedirs(student_dir) #创建多级目录
-    except FileExistsError:
-        pass
-    #将文件解压至文件夹中
-    #zip_object = zipfile.ZipFile(zip_file)
-    #zip_object.extractall(student_dir)
-
-    # 搜索相应学号文件夹的.java, .pdf文件并放入相应学号文件夹中
-    types = ['*.pdf', '*.java']
-    for type in types:
-        file_list = glob.glob(student_dir + '/**/' + type, recursive=True) # 对file_list进行for循环, search/app/src/
-        for file_path in file_list:
-            if(check_is_not_MainActivity(type, file_path)): #如果不是在app/src目录下的.java文件, continue
-                continue
-            try:
-                shutil.copy(file_path, student_dir)
-            except shutil.SameFileError: #如果目录一一致, 则跳过
-                pass
-    #对student_dir下进行list, 如果有
-    sub_file_list = os.listdir(student_dir)
-    delete_dir(student_dir, sub_file_list)
-'''
-
-
-
 
 
 class Tools:
     def __init__(self):
-        self.base_path = None # 存放压缩文件的基本路径
-        self._dist_path = None # 作业放入的目的地址
-        self._search_path = None # 搜索路径
-        self._student_list = dict() # 学生列表 str->bool
+        self._dist_path = None  # 作业放入的目的地址
+        self._search_path = None  # 搜索路径
+        self._student_list = dict()  # 学生列表 str->bool
 
-    def create_dist_dir(self, path): # 传入一个路径, 创建相应的dir
-        if(os.path.exists(path)):#检测路径文件是否存在
-            print('该路径已经存在, 如果继续使用该路径输入yes,  否则程序终止')
-            if(not input() == 'yes'):
-                pass
-                #return #如果输入的不是yes则直接返回
-            # shutil.rmtree(path) #删除掉该路径
-        self._dist_path = path # 同时保存作业地址
+    def create_dist_dir(self, path):  # 传入一个路径, 创建相应的dir
+        if(os.path.exists(path)):  # 检测路径文件是否存在
+            print('该路径已经存在, 如果删除该路径输入yes,  否则继续')
+            if(input() == 'yes'):
+                shutil.rmtree(path)  # 删除掉该路径
+        self._dist_path = path  # 同时保存作业地址
         try:
             os.makedirs(path)
         except:
             pass
+
+    '''
+    通过读取excel表格初始化学生数据
+    path : excel表格路径
+    col : 学号所在的列号
+    start_row : 读取的起始行号
+    '''
+    # 初始化学生列表, 传入excel路径, 与学号所在的列, 和起始的行数
+    def init_student_list(self, path, col=0, start_row=0):
+        excel_data = xlrd.open_workbook(path)
+        sh = excel_data.sheet_by_index(0)
+        for row in range(start_row, sh.nrows):
+            data = sh.cell(row, col).value
+            self._student_list[data] = False
+
 
     # search_dir get and set
     def set_search_dir(self, path):
@@ -84,66 +46,26 @@ class Tools:
     def get_search_dir(self):
         self._search_path
 
-    def init_student_list(self, path): # 初始化学生列表
-        excel_data = xlrd.open_workbook(path)
-        sh = excel_data.sheet_by_index(0)
-        for row in range(1, sh.nrows):
-            data = sh.cell(row, 0).value
-            self._student_list[data] = False
-
-    def _get_specific_file_list(self, recursive): #获取文件列表
-        return glob.glob(self._search_path, recursive=recursive)  # 是否递归
-
-    def _get_sid(self, file_list):
-        regex_pattern = re.compile(r'\d{8}')
-        result = list()
-        for zip_file in file_list:
-            # 首先创建相应学号的文件夹
-            sid = regex_pattern.findall(zip_file)
-            if (not len(sid) == 1):  # 如果获取到的sid不等于1
-                print('正则匹配出现异常, 该文件名为 {}'.format(zip_file))
-                continue
-            result.append(sid[0])
-        return result
-
-    def get_homework_result(self, paths):
-        student_file_list = []
-        for path in paths:
-            self.set_search_dir(path)
-            student_file_list.extend(self._get_specific_file_list(True))
+    '''
+    返回检测作业上交情况
+    paths : 搜索的文件夹目录, 不支持递归, 仅在文件夹内搜索
+    '''
+    def get_homework_result(self, path):  # 这里直接列出所有文件即可
+        student_file_list = os.listdir(path)  # 列出该目录下所有文件
         student_list = self._get_sid(student_file_list)
         for student in student_list:
-                #assert student in self._student_list
             self._student_list[student] = True
         result = [key for key, value in self._student_list.items() if value == False]
         return result
 
-
-    def unpack_zip(self, path):
-        self.set_search_dir(path)
-        assert self._dist_path != None # 先设置好dist_path
-        homework_list = self._get_specific_file_list(True) #获取所有zip列表
-        regex_pattern = re.compile(r'\d{8}') #定义正则pattern
-        for zip_file in homework_list:
-            # 首先创建相应学号的文件夹
-            sid = regex_pattern.findall(zip_file)
-            if (not len(sid) == 1):  # 如果获取到的sid不等于1
-                print('正则匹配出现异常, 该文件名为 {}'.format(zip_file))
-                continue
-
-            # 指定文件中创建目录
-            student_dir = self._dist_path + '/' + sid[0]
-            try:
-                os.makedirs(student_dir)  # 创建多级目录
-            except FileExistsError: # 文件已经存在
-                pass
-            # 将文件解压至文件夹中
-            zip_object = zipfile.ZipFile(zip_file)
-            zip_object.extractall(student_dir)
-
-
-    def copy_specific_type(self, path, remove=False, **kwargs): # 传入解压后, 作业放入的base_dir
-
+    '''
+    解压后, 拷贝相应的后缀文件到学生文件根目录下,
+    path : 解压后文件夹所在的根目录
+    remove : 拷贝完成后是否删除文件夹
+    kwargs-types : 需要拷贝的文件类型
+    kwargs-exclude :  补需要拷贝的文件
+    '''
+    def copy_specific_type(self, path, remove=False, **kwargs):
         student_list = os.listdir(path)
         types = kwargs['types']
         exclude = kwargs['exclude']
@@ -162,28 +84,52 @@ class Tools:
             if remove == True: #如果为remove = True, 删除文件夹
                 self.delete_dir(student_dir)
 
+    '''
+    解压rar文件到指定目录
+    path : 解压的目的目录
+    types : 解压文件类型
+    '''
+    def unpack(self, path, types=['*.zip', '*.rar']):
+        for type in types:
+            assert self._dist_path != None  # 先设置好dist_path
+            self._search_path = self._set_glob_search_file(path, type)
+            files = self._get_specific_file_list(True)  # 获取所有rar列表
+            regex_pattern = re.compile(r'\d{8}')  # 定义正则pattern
+            for file in files:
+                # 其实这里是否需要加入assert
+                print(file + '    正在解压')
+                sid = regex_pattern.findall(file)
+                if (not len(sid) == 1):  # 如果获取到的sid不等于1
+                    print('正则匹配出现异常, 该文件名为 {}'.format(file))
+                    continue
+                student_dir = self._dist_path + '/' + sid[0]
+                try:
+                    os.makedirs(student_dir)  # 创建多级目录
+                except FileExistsError:  # 文件已经存在
+                    pass
+                try:
+                    compressed_object = rarfile.RarFile(file) if file.endswith('.rar') else zipfile.ZipFile(file) # 可拓展性不强
+                    compressed_object.extractall(student_dir)
+                except:  # 避免一些错误的压缩文件
+                    pass
 
-    def unpack_rar(self, path):
-        self.set_search_dir(path)
-        assert self._dist_path != None  # 先设置好dist_path
-        rar_files = self._get_specific_file_list(True)  # 获取所有rar列表
-        regex_pattern = re.compile(r'\d{8}')  # 定义正则pattern
-        for rar_file in rar_files:
-            rar_object = rarfile.RarFile(rar_file)
-            sid = regex_pattern.findall(rar_file)
-            if (not len(sid) == 1):  # 如果获取到的sid不等于1
-                print('正则匹配出现异常, 该文件名为 {}'.format(rar_file))
-                continue
-            student_dir = self._dist_path + '/' + sid[0]
-            try:
-                os.makedirs(student_dir)  # 创建多级目录
-            except FileExistsError: # 文件已经存在
-                pass
-            rar_object.extractall(student_dir)
+    def delete_dir(self, path):
+        sub_file_list = os.listdir(path)
+        for sub_file in sub_file_list:  # 注意列出来的只是一个文件名, 而不是一个目录
+            if os.path.isdir(os.path.join(path, sub_file)):  # 如果是目录, 则删除,
+                shutil.rmtree(os.path.join(path, sub_file))
 
+    '''
+    设置glob搜索路径
+    path : 搜索根文件
+    file_type : 搜索的文件类型
+    '''
+    def _set_glob_search_file(self, path, file_type):
+        return os.path.join(path, '**', file_type) # 方便跨平台
 
-    def _get_all_specific_file(self):
-        pass
+    # 文件搜索, 参数recursive表示是否递归
+    def _get_specific_file_list(self, recursive): #获取文件列表
+        return glob.glob(self._search_path, recursive=recursive)  # 是否递归
 
     def _check_is_not_MainActivity(self, type, file_path, specific_dir = None, exclude = []): # 写得有点问题
         if(specific_dir != None and type == '*.java'):
@@ -194,8 +140,17 @@ class Tools:
                 return True
         return False
 
-    def delete_dir(self, path):
-        sub_file_list = os.listdir(path)
-        for sub_file in sub_file_list:  # 注意列出来的只是一个文件名, 而不是一个目录
-            if os.path.isdir(path + '/' + sub_file):  # 如果是目录, 则删除,
-                shutil.rmtree(path + '/' + sub_file)
+    '''
+    获取学生学号列表
+    '''
+    def _get_sid(self, file_list):
+        regex_pattern = re.compile(r'\d{8}')
+        result = list()
+        for zip_file in file_list:
+            # 首先创建相应学号的文件夹
+            sid = regex_pattern.findall(zip_file)
+            if (not len(sid) == 1):  # 如果获取到的sid不等于1
+                print('正则匹配出现异常, 该文件名为 {}'.format(zip_file))
+                continue
+            result.append(sid[0])
+        return result
